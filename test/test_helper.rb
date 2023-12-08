7218e87c3f102830d512b81cc3a4eccda19132dc
aaron <aaronnsibanda@gmail.com>
2023-12-08 10:11:34 +0100

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,9 +11,9 @@ module ActiveSupport
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
     include ApplicationHelper
-
     # Add more helper methods to be used by all tests here...
-
-    
+    def is_logged_in?
+      !session[:user_id].nil?
+    end    
   end
 end
