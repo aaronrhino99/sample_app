c93b9f59aa4c8eecd60f1804a5ceffe989e62ca7
aaron <aaronnsibanda@gmail.com>
2023-12-22 02:26:21 +0100

    Add password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  host = 'example.com' # Dont use this literally; use you local host instead
+  host = 'http://localhost:3000/' #  local host
 
   config.action_mailer.default_url_options = {host: host, protocol: 'http'}
 
